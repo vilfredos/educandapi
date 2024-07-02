5a94f865a3837697ab745693ffa1df8b32399bd7
vilfredos <103297447+vilfredos@users.noreply.github.com>
2024-07-02 10:27:46 -0400

    Actualizaciones y

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>UMSS</title>
+    <title>UMSSds</title>
     <link href="{{ asset('css/welcome.css') }}" rel="stylesheet">
 </head>
 
